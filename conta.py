5dcee31aff379de4bbae7e6758830e65780311f1
João Victor Mesquita <jmesquit@isi-montreal.com>
2021-01-30 16:55:15 -0500

    criação classe conta

---
+++ conta.py
@@ -0,0 +1,6 @@
+class Conta:
+    def __init__(self, conta, titular, saldo, limite):
+        self.conta = conta
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
